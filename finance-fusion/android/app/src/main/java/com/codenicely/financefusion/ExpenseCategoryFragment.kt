0a16e5a3892c96a1bd2bf300619e97af7cf1765e
Ujjwal Agrawal <ujjwal@codenicely.in>
2023-09-16 20:23:14 +0530

    Added Initial UI screens for finance-fusion Android project created using GPT

---
+++ finance-fusion/android/app/src/main/java/com/codenicely/financefusion/ExpenseCategoryFragment.kt
@@ -0,0 +1,31 @@
+package com.codenicely.financefusion
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_expense_category.*
+
+class ExpenseCategoryFragment : Fragment() {
+
+    private lateinit var adapter: ExpenseCategoryAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_expense_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        adapter = ExpenseCategoryAdapter()
+        recyclerView.adapter = adapter
+
+        // TODO: Load expense categories
+    }
+}
